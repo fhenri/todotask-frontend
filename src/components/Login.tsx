82c62c76db6274fae365dd3064d697736eb08ce6
fhenri <fhenri@fhsolution.com>
2024-06-17 18:39:52 +0200

    add nav bottom and fix error message on login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -30,7 +30,6 @@ const Login: React.FC = () => {
     <h1 className="mb-8 font-extrabold text-4xl">Login</h1>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-    {error && <p style={{ color: 'red' }}>{error}</p>}
 
         <form onSubmit={handleLogin}>
             <div>
@@ -48,6 +47,7 @@ const Login: React.FC = () => {
             </div>
 
             <div className="flex items-center justify-between mt-8">
+                <span className="text-red-500 font-bold text-xl">{error}</span>
                 <button type="submit"
                         className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">Login</button>
             </div>
@@ -57,8 +57,8 @@ const Login: React.FC = () => {
             <div className="bg-gray-100 p-8 rounded">
                 <h2 className="font-bold text-2xl">Instructions</h2>
                 <ul className="list-disc mt-4 list-inside">
-                    <li>This is not an open app.</li>
-                    <li>You can use the <span className="font-bold">tododemo / tododemo</span> account to test the app</li>
+                    <li>This is not an open app, send an email to fhenri@cloud06.io if you require real account.</li>
+                    <li>You can use the <span className="font-bold">tododemo / tododemo</span> account to test the app.</li>
                     <li>All data from the tododemo account are removed on daily basis, demo account also cannot send mails.</li>
                 </ul>
             </div>
